24a710100268d73249cc951e7cc391eb897e2cab
Maxim Sakharov <sakharov.ma@sberleasing.ru>
2022-11-16 14:10:27 -0600

    changes to ShopListRepositoryImpl

--- app/src/main/java/com/example/shoppinglistapp/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglistapp/data/ShopListRepositoryImpl.kt
@@ -10,6 +10,12 @@ object ShopListRepositoryImpl : ShopListRepository {
 
     private var autoIncrementId = 0
 
+    init {
+        for (i in 0 until 10) {
+            shopList.add(ShopItem("Name $i", i, true))
+        }
+    }
+
     override fun addShopItem(shopItem: ShopItem) {
         if(shopItem.id == ShopItem.UNDEFINED_ID ) {
             shopItem.id = autoIncrementId++
